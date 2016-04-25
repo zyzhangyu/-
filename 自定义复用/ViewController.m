a8ccb7fad482affcc71446afd037659436756962
张宇 <zylike@Hotmail.com>
2016-04-25 09:25:01 +0800

    添加初始项目
    使用XCode创建了一个工程项目

---
+++ 自定义复用/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  自定义复用
+//
+//  Created by ZhangYu on 16/4/25.
+//  Copyright © 2016年 ZhangYu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 自定义复用/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  自定义复用
+//
+//  Created by ZhangYu on 16/4/25.
+//  Copyright © 2016年 ZhangYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
