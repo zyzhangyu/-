063ca4f034daaa4bed008d382164b02aa9fe1441
张宇1 <zylike@Hotmail.com>
2021-01-07 13:44:39 +0800

    1.添加对ts的支持完毕
    修改了_app.js 变换成_app.tsx
    修改了_index.js变换成index.tsx

---
+++ myblog/pages/index.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+import Head from "next/head";
+
+
+
+const Home: FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      123123
+    </div>
+  );
+};
+
+export default Home;
